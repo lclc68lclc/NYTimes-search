e5cb5743f2d863689bea56abc18a9099a9a52506
Lina C <linaclark96@gmail.com>
2017-09-16 14:00:21 -0400

    update

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
     $("#search").on("click", function() {
         var searchTerm = $("#search-term").val();
-        var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json&api_key=053d4d43c9d0490185abb660e1352122";
+        var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+        url += '?' + $.param({
+            'api-key': "053d4d43c9d0490185abb660e1352122"
+        });
         $.ajax({
             url: url,
             method: 'GET',
